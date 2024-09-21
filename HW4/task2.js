cb0105435c7d2b1c5074e879490195f5e312f45f
maksyuta <s.maksyuta1@gmail.com>
2024-09-21 23:18:38 +0300

    add homeworks

--- HW4/task2.js
+++ HW4/task2.js
@@ -1,77 +1,38 @@
-// 1.
-// Реализовать Task 1 через switch
+// Task 2
+// Работа с оператором IF:
 
-const age = 10;
-const age_2 = 18;
-const age_3 = 60;
+//   1. Создать переменную "minAge" и присвоить ей значение 18
+const minAge = 18;
+//   2. Создать переменную "maxAge" и присвоить ей значение 60
+const maxAge = 60;
+//   3. Создать переменную "age", в которую вы будете вводить возраст при выполнении программы
+let age = 61;
+//   4. Добавьте проверку: если тип данных в переменной age не number - вывести в консоль Incorrect data type
+//   4. Создать if в котором будете проверять весь код переменной age со следующими условиями:
+//     - Если age меньше чем minAge, вывести в консоль "You don't have access cause your age is " + "age" + " It's less then "
+//     - Если "age" больше либо равно  minAge и меньше  maxAge, вывести в консоль "Welcome  !"
+//     - Если "age" больше maxAge, вывести в консоль "Keep calm and look Culture channel".
+//     - Иначе выводите "Technical work".
+//   5. Проверить задание со следующими значениями в переменной age: 10, 17, 18, 19, 59, 60, 61
 
-switch (true) {
-    case (age < age_2): {
-        console.log(`You don’t have access cause, your age is ${age}. It’s less then ${age_2}`);
-        break;
-    }
-    case (age >= age_2 && age <= age_3): {
-        console.log("Welcome  !");
-        break;
-    }
-    case (age > age_3): {
-        console.log("Keep calm and look Culture channel");
-        break;
-    }
-    default: {
-        console.log("Technical work")
-    }
-}
 
-// 2.
-// Преобразовать написанный код в task 1 так, чтобы сначала проверялся тип данных.
-//   И если он не number - кидалась ошибка в консоль.
-//   Проверить работу кода на следующих данных 17, 18, 61, "2", "aaa"
 
-const ages = [17, 18, 61, "2", "aaa"];
-const age_2 = 18;
-const age_3 = 60;
 
-for (let i = 0; i < ages.length; i++) {
-    checkAge(ages[i]);
-}
-
-function checkAge(age) {
-    if (typeof age !== 'number') {
-        console.log(`Invalid value - Not a number!`);
-    } else if (age < age_2) {
-        console.log(`You don’t have access cause your age is ${age}. It’s less than ${age_2}`);
-    } else if (age >= age_2 && age <= age_3) {
-        console.log('Welcome!');
-    } else if (age > age_3) {
-        console.log('Keep calm and watch the Culture channel');
+function chackAge(age) {
+    if(typeof age !== 'number'){
+        console.log('Incorrect data type')
     } else {
-        console.log('Technical work');
-    }
-}
-
-
-// 3.
-// Преобразовать Task 2 - 2 таким образом, чтобы значение НАПРИМЕР '2' (т.е. ЛЮБАЯ строка в которой лежат ТОЛЬКО ЦИФРЫ) пропускалось,
-//   преобразовываясь в number
-
-const ages = [17, 18, 61, "2", "aaa"];
-const age_2 = 18;
-const age_3 = 60;
-
-ages.forEach(age => checkAge(age));
-
-function checkAge(age) {
-    age = Number(age);
-    if (isNaN(age)) {
-        console.log(`Invalid value - Not a number!`);
-    } else if (age < age_2) {
-        console.log(`You don’t have access cause your age is ${age}. It’s less than ${age_2}`);
-    } else if (age >= age_2 && age <= age_3) {
-        console.log('Welcome!');
-    } else if (age > age_3) {
-        console.log('Keep calm and watch the Culture channel');
+    if (age < minAge) {
+        console.log(`You don’t have access cause, your age is ${age}. It’s less then ${minAge}`);
+    } else if (age >= minAge && age < maxAge) {
+        console.log('Welcome !')
+    } else if (age > maxAge)
+    {
+        console.log('Keep calm and look Culture channel')
     } else {
-        console.log('Technical work');
+        console.log('Technical work')
     }
 }
+}
+
+chackAge(age)
